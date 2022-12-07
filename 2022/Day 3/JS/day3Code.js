9dc1e7d89ae581d54337d95b11295ddb989d608e
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2022-12-07 11:50:10 +0000

    add -1 to testString split function

--- 2022/Day 3/JS/day3Code.js
+++ 2022/Day 3/JS/day3Code.js
@@ -6,7 +6,7 @@ fs.readFile("../2022/Day 3/input.txt", "utf8", (err, data) => {
   if (err) throw err;
   dataToString = data.toString().split("\r\n");
   // console.log(dataToString);
-  for (let rucksack = 0; rucksack < (dataToString.length)/2; rucksack++) {
+  for (let rucksack = 0; rucksack < dataToString.length; rucksack++) {
     let compartment1 = []
     let compartment2 = []
   }
@@ -15,14 +15,21 @@ fs.readFile("../2022/Day 3/input.txt", "utf8", (err, data) => {
 const itemTypes = () => {
   let compartment1 = []
   let compartment2 = []
-  for (let i = 0; i < (testString.length); i++) {
-    if (i <= (testString.length)/2) {
-      compartment1.push(testString[i])
+  for (let item = 0; item < (testString.length); item++) {
+    if (item <= (testString.length)/2) {
+      compartment1.push(testString[item])
     } else {
-      compartment2.push(testString[i])
+      compartment2.push(testString[item])
     }
   }
-  console.log(compartment2[1])
+  console.log(compartment1.length, 'vs', compartment2.length)
+
+  // https://www.w3schools.com/jsref/jsref_indexof_array.asp
+  for (let itemToCompare = 0; itemToCompare < compartment1.length; itemToCompare++) {
+    if (compartment2.indexOf(compartment1[itemToCompare]) !== -1) {
+      // console.log(compartment1[itemToCompare])
+    }
+  }
 }
 
 itemTypes()
