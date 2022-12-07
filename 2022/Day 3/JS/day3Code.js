2786f6728d1202fa888b2ed5b9c5ab4656569959
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2022-12-07 11:38:38 +0000

    creating the compartments for comparison

--- 2022/Day 3/JS/day3Code.js
+++ 2022/Day 3/JS/day3Code.js
@@ -1,10 +1,28 @@
+testString = 'BccTFfTPTsffdDDqsq'
+
 const fs = require("fs");
 
 fs.readFile("../2022/Day 3/input.txt", "utf8", (err, data) => {
   if (err) throw err;
   dataToString = data.toString().split("\r\n");
   // console.log(dataToString);
-  for (let rucksack = 0; rucksack < dataToString.length; rucksack++) {
-    
+  for (let rucksack = 0; rucksack < (dataToString.length)/2; rucksack++) {
+    let compartment1 = []
+    let compartment2 = []
   }
 });
+
+const itemTypes = () => {
+  let compartment1 = []
+  let compartment2 = []
+  for (let i = 0; i < (testString.length); i++) {
+    if (i <= (testString.length)/2) {
+      compartment1.push(testString[i])
+    } else {
+      compartment2.push(testString[i])
+    }
+  }
+  console.log(compartment2[1])
+}
+
+itemTypes()
