e2af0c0d4f8ab2fbcd7434bf650446bfb850261e
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2022-12-08 00:42:51 +0000

    start of priority sum

--- 2022/Day 3/JS/day3Code.js
+++ 2022/Day 3/JS/day3Code.js
@@ -1,4 +1,4 @@
-testString = 'BccTFfTPTsffdDDqsq'
+testString = "BccTFfTPTsffdDDqsq";
 
 const fs = require("fs");
 
@@ -6,32 +6,43 @@ fs.readFile("../2022/Day 3/input.txt", "utf8", (err, data) => {
   if (err) throw err;
   dataToString = data.toString().split("\r\n");
   // console.log(dataToString);
-  for (let rucksack = 0; rucksack < dataToString.length; rucksack++) {
-    let compartment1 = []
-    let compartment2 = []
-  }
 });
 
 const itemType = (string) => {
-  let compartment1 = []
-  let compartment2 = []
+  let compartment1 = [];
+  let compartment2 = [];
+  let match = "";
 
   // First split the string into two equal halves
-  for (let item = 0; item < (string.length); item++) {
-    if (item <= ((string.length / 2) - 1)) {
-      compartment1.push(string[item])
+  for (let item = 0; item < string.length; item++) {
+    if (item <= string.length / 2 - 1) {
+      compartment1.push(string[item]);
     } else {
-      compartment2.push(string[item])
+      compartment2.push(string[item]);
     }
   }
 
   // Use indexOf to find the matching item
   // https://www.w3schools.com/jsref/jsref_indexof_array.asp
-  for (let itemToCompare = 0; itemToCompare < compartment1.length; itemToCompare++) {
+  for (
+    let itemToCompare = 0;
+    itemToCompare < compartment1.length;
+    itemToCompare++
+  ) {
     if (compartment2.indexOf(compartment1[itemToCompare]) !== -1) {
-      console.log(compartment1[itemToCompare])
+      match += compartment1[itemToCompare];
+      // console.log("the match is ", match);
     }
   }
-}
+  return match;
+};
 
-itemType(testString)
+// Find the sum of the priority of the matching items
+const prioritySum = (string) => {
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+prioritySum = 0;
+
+  const match = itemType(string);
+};
+
+prioritySum(testString);
