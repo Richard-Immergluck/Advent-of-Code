eeb6c403c2003e33491a364e5eb447eec2e04cd5
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-01-23 21:05:16 +0000

    Finished Day 3

--- 2022/Day 3/JS/day3Code.js
+++ 2022/Day 3/JS/day3Code.js
@@ -52,23 +52,88 @@ const testCase = (char) => {
 // Part 1
 // Find the sum of the priority of the matching items
 const prioritySum = (array) => {
-  let prioritySum = 0;
+  let sum = 0;
   for (let rucksack = 0; rucksack < array.length; rucksack++) {
     let match = itemType(array[rucksack]);
     if (testCase(match) === true) {
-      prioritySum += match.charCodeAt(0) - 38;
+      sum += match.charCodeAt(0) - 38;
     } else {
-      prioritySum += match.charCodeAt(0) - 96;
+      sum += match.charCodeAt(0) - 96;
     }
   }
-  console.log(prioritySum)
+  console.log(sum);
 };
 
 // Part 2
+
+// Seperate into groups of 3 rucksacks
+const splitIntoGroups = (array) => {
+  let group = [];
+  for (let rucksack = 0; rucksack < array.length; rucksack++) {
+    if (rucksack % 3 === 0) {
+      group.push([array[rucksack]]);
+    } else {
+      group[group.length - 1].push(array[rucksack]);
+    }
+  }
+  return group;
+};
+
+const finalSum = (array) => {
+  let sum = 0;
+  for (let item = 0; item < array.length; item++) {
+    if (testCase(array[item]) === true) {
+      sum += array[item].charCodeAt(0) - 38;
+    } else {
+      sum += array[item].charCodeAt(0) - 96;
+    }
+  }
+  return sum;
+};
+
+const threeBagMatch = (group) => {
+  const bagOne = group[0];
+  const bagTwo = group[1];
+  const bagThree = group[2];
+  let match = "";
+
+  console.log("Bag One", bagOne);
+  console.log("Bag Two", bagTwo);
+  console.log("Bag Three", bagThree);
+
+  // Run an iterative comparison function to check each item in the first rucksack
+  // against the items in the second and third rucksacks.
+  for (let itemToCompare = 0; itemToCompare < bagOne.length; itemToCompare++) {
+    console.log('letter being compared is ====> ', bagOne[itemToCompare]);
+    if (match === "") {
+      // If the letter in bag one matches the letter in bag two
+      console.log(bagTwo.indexOf(bagOne[itemToCompare]) !=-1 )
+      if (bagTwo.indexOf(bagOne[itemToCompare]) !== -1) {
+        // If the letter in bag one matches the letter in bag three
+        if (bagThree.indexOf(bagOne[itemToCompare]) !== -1) {
+          match = bagOne[itemToCompare];
+          console.log('match found', match);
+        }
+      }
+    }
+  }
+  return match;
+};
+
 // Find the sum of the priorities that appeear in each group of 3 rucksacks
-const prioritySum3 = (array) => {
-  let prioritySum = 0;
+const prioritySumOf3 = (array) => {
+  let matchArray = [];
+  let sum = 0;
+  const groups = splitIntoGroups(array);
+  for (
+    let individualGroup = 0;
+    individualGroup < groups.length;
+    individualGroup++
+  ) {
+    matchArray.push(threeBagMatch(groups[individualGroup]));
+  }
+  console.log(finalSum(matchArray));
+};
 
-}
-
-prioritySum(dataArray);
+// prioritySum(dataArray);
+prioritySumOf3(dataArray);
