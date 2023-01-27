4370ea0cbb9e655a0cda0b93c3ab63fc153aa693
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-01-27 14:35:12 +0000

    Setting up day 5

---
+++ 2022/Day 5/JS/day5code.js
@@ -0,0 +1,11 @@
+
+// Columns 
+let column1 = 'SZPDLBFC'
+let column2 = 'NVGPHWB'
+let column3 = 'FWBJG'
+let column4 = 'GJNFLWCS'
+let column5 = 'WJLTPMSH'
+let column6 = 'BCWGFS'
+let column7 = 'HTPMQBW'
+let column8 = 'FSWT'
+let column9 = 'NCR'
