46da2979f7aef9bfe1d2fdd6d09113ab78b2661e
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-01-27 22:58:06 +0000

    Work day5 part 1 but not giving right answer

--- 2022/Day 5/JS/day5code.js
+++ 2022/Day 5/JS/day5code.js
@@ -4,34 +4,96 @@ const fs = require("fs");
 const inputFileData = fs.readFileSync("../2022/Day 5/moves.txt", "utf8");
 dataArray = inputFileData.split("\r\n");
 
-console.log(dataArray);
+testArray = [
+  "move 2 from 5 to 9",
+  "move 3 from 1 to 7",
+  "move 2 from 3 to 9",
+  "move 6 from 9 to 5",
+  "move 2 from 3 to 8",
+  "move 9 from 7 to 8",
+  "move 15 from 8 to 9",
+  "move 3 from 1 to 6",
+  "move 6 from 4 to 2",
+  "move 6 from 5 to 6",
+  "move 1 from 4 to 2",
+  "move 14 from 6 to 2",
+  "move 2 from 1 to 5",
+  "move 1 from 7 to 3",
+  "move 1 from 4 to 8",
+  "move 2 from 5 to 6",
+  "move 25 from 2 to 4",
+  "move 2 from 6 to 4",
+];
 
-// Columns
-let column1 = "SZPDLBFC";
-let column2 = "NVGPHWB";
-let column3 = "FWBJG";
-let column4 = "GJNFLWCS";
-let column5 = "WJLTPMSH";
-let column6 = "BCWGFS";
-let column7 = "HTPMQBW";
-let column8 = "FSWT";
-let column9 = "NCR";
+// console.log(dataArray);
 
 // We've got 3 input numbers. The number of crates to move, a FROM and a TO
 
 const crateSorter = (dataArray) => {
-  
+  // Columns as an array of objects
+  let columns = [
+    { id: 1, contents: "SZPDLBFC" },
+    { id: 2, contents: "NVGPHWB" },
+    { id: 3, contents: "FWBJG" },
+    { id: 4, contents: "GJNFLWCS" },
+    { id: 5, contents: "WJLTPMSH" },
+    { id: 6, contents: "BCWGFS" },
+    { id: 7, contents: "HTPMQBW" },
+    { id: 8, contents: "FSWT" },
+    { id: 9, contents: "NCR" },
+  ];
+
   // Loop through the data array
-  for (let instruction = 0; instruction <= dataArray.length; instruction++) {
+  for (let instruction = 0; instruction < dataArray.length; instruction++) {
+    // Establish the variables to store the relevant data
+    let numberOfCrates = 0;
+    let from = null;
+    let to = null;
+
+    // extract the 3 relevant pieces of data and store in variables
     let numbers = dataArray[instruction].match(/\d+/g);
-    // extract the 3 relevant pieces of data
-    console.log(numbers);
+    numberOfCrates = numbers[0];
+    from = numbers[1];
+    to = numbers[2];
+
+    console.log(
+      `For instruction ${instruction + 1}, the number of crates to move
+      is ${numberOfCrates}, the FROM column is ${from} (${
+        columns[from - 1].contents
+      })
+      and the TO column is ${to} (${columns[to - 1].contents})`
+    );
+
+    // Get the FROM column letters
+    let pushedLetters = columns[from - 1].contents.slice(
+      columns[from - 1].contents.length - numberOfCrates
+    );
+    // console.log("pushed letters are ====> ", pushedLetters);
+
+    // Get the TO column letters and concat the sliced letters from the FROM column
+    let result = columns[to - 1].contents.concat(pushedLetters);
+
+    // Update the contents of the TO column
+    columns[to - 1].contents = result;
+
+    // Finally update the FROM column
+    columns[from - 1].contents = columns[from - 1].contents.slice(
+      0,
+      columns[from - 1].contents.length - numberOfCrates
+    );
+    console.log("Confirm FROM column updated", columns[from - 1]);
+    console.log("confirm TO column updated", columns[to - 1]);
   }
 
-  // manipulate the columns accordingly.
-  let numberOfCrates = 0;
-  let from = null;
-  let two = null;
+  // Once the for loop has finished, find the top crate in each stack.
+  for (let stackIndex = 0; stackIndex <= 8; stackIndex++) {
+    console.log('Full stack', columns[stackIndex].contents)
+    console.log(
+      columns[stackIndex].contents.slice(
+        columns[stackIndex].contents.length - 1
+      )
+    );
+  }
 };
 
 crateSorter(dataArray);
