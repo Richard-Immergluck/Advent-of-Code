9cf006aeb107eaeb01aabb853ff224005940b847
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-01-27 14:26:04 +0000

    Day 4 completed

--- 2022/Day 4/JS/day4code.js
+++ 2022/Day 4/JS/day4code.js
@@ -1,31 +1,30 @@
 const fs = require("fs");
 
-const testArray = [
-  "31-31,32-40",
-  "26-92,13-91",
-  "9-90,29-91",
-  "72-72,25-73",
-  "28-79,79-79",
-  "52-77,53-53",
-];
+// const testArray = [
+//   "31-31,32-40",
+//   "26-92,13-91",
+//   "9-90,29-91",
+//   "72-72,25-73",
+//   "28-79,79-79",
+//   "52-77,53-53",
+// ];
 
 const inputFileData = fs.readFileSync("../2022/Day 4/input.txt", "utf8");
 dataArray = inputFileData.split("\r\n");
 
-const containedPairFinder = (testArray) => {
+const containedPairFinder = (dataArray) => {
   let tally = 0;
-  for (let arrayPair = 0; arrayPair < testArray.length; arrayPair++) {
-
-    console.log('Current array pair is =====>', testArray[arrayPair])
+  for (let arrayPair = 0; arrayPair < dataArray.length; arrayPair++) {
+    console.log("Current array pair is =====>", dataArray[arrayPair]);
 
     let A = 0;
     let B = 0;
     let X = 0;
     let Y = 0;
 
-    splitArray = testArray[arrayPair].split(",");
-    console.log('Split Array pair is ====> ', splitArray)
-    
+    splitArray = dataArray[arrayPair].split(",");
+    console.log("Split Array pair is ====> ", splitArray);
+
     ABnumbers = splitArray[0].split("-");
     A = +ABnumbers[0];
     B = +ABnumbers[1];
@@ -33,24 +32,33 @@ const containedPairFinder = (testArray) => {
     X = +XYnumbers[0];
     Y = +XYnumbers[1];
 
-    console.log(`The left pair are ${A}, amd ${B} ---- The Right pair are ${X} and ${Y}`)
+    console.log(
+      `The left pair are ${A}, amd ${B} ---- The Right pair are ${X} and ${Y}`
+    );
 
-    // // right group contains left group
-    // // left group contains right group
-    // // if right group contain only one field
-    // // if left group contains online one field
+    // Part 1 conditionals
+    // right group contains left group
+    // left group contains right group
+    // specific conditional for if right group contain only one field
+    // specific conditional for if left group contains only one field
+
+    // Part 2 conditionals
+    // Right group overlaps left group
+    // Left group overlaps right group?? Is this necessary --- yes it is
 
     // Conditionals as one line
     if (
-      (A <= X && B >= Y) ||
-      (X <= A && Y >= B) ||
-      (X === Y && A <= X && X <= B) ||
-      (A === B && X <= A && A <= Y)
+      (A <= X && B >= Y) || // part 1
+      (X <= A && Y >= B) || // part 1
+      (X === Y && A <= X && X <= B) || // part 1
+      (A === B && X <= A && A <= Y) || // part 1
+      (A <= X && B >= X) || // part 2 
+      (X <= A && Y >= A) // part 2
     ) {
-      console.log("contained pair found!!!", testArray[arrayPair]);
-    tally++;
-    console.log(tally)
-  }
+      console.log("contained pair found!!!", dataArray[arrayPair]);
+      tally++;
+      console.log(tally);
+    }
   }
   console.log(tally);
 };
