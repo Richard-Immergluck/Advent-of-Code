79bebf88305f78991f1ce66c686e4b7ffe14106a
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-02-01 10:20:42 +0000

    string split into sections of 4 moving up string

--- 2022/Day 6/JS/day6Code.js
+++ 2022/Day 6/JS/day6Code.js
@@ -14,11 +14,33 @@ up the string one character at a time.
 
 */
 
-// Test cases 
-const testCase1 = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+// Test cases
+const testCase1 = "bvwbjplbgvbhsrlpgdmjqwftvncz";
 // first marker after character 5
 
-const testCase2 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+const testCase2 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
 // first marker after character 10
 
+const fs = require("fs");
 
+packet = fs.readFileSync("../2022/Day 6/input.txt", "utf8").split();
+
+const packetStartMarker = (packet) => {
+  // initialise required variables
+  let sequence = "";
+
+  // First loop to act as the head of the sequence
+  for (let head = 4; head < packet.length; head++) {
+    // Second loop to act as the tail of the sequence
+    for (let tail = head - 4; tail < head; tail++) {
+      sequence += packet[tail]
+    }
+    
+    // Find the four different characters function should be in here. 
+
+    console.log(`updating sequence`, sequence)
+    sequence = ""
+  }
+};
+
+packetStartMarker(testCase1);
