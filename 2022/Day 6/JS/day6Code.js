2a64ed55a7fbb79634ac1dd7683ebb4ca4d0541e
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-02-01 09:49:10 +0000

    Starting day 6

---
+++ 2022/Day 6/JS/day6Code.js
@@ -0,0 +1,24 @@
+/* 
+
+--- Day 6: Tuning Trouble ---
+
+*/
+
+/* 
+
+We have a string and we need to find the first 4 characters
+within the string that are not repeated. 
+The way I see this happening is with two loops pulling in the
+relevant characters and then comparing them to each other. Moving
+up the string one character at a time. 
+
+*/
+
+// Test cases 
+const testCase1 = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+// first marker after character 5
+
+const testCase2 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+// first marker after character 10
+
+
