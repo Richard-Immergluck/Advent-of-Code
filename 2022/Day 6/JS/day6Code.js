0c0e72e53f7bfa87648c3b256022e4f53213dcba
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-02-06 16:05:45 +0000

    Part 1 completed

--- 2022/Day 6/JS/day6Code.js
+++ 2022/Day 6/JS/day6Code.js
@@ -2,9 +2,6 @@
 
 --- Day 6: Tuning Trouble ---
 
-*/
-
-/* 
 We have a string and we need to find the first 4 characters
 within the string that are not repeated. 
 The way I see this happening is with two loops pulling in the
@@ -20,54 +17,96 @@ const testCase1 = "bvwbjplbgvbhsrlpgdmjqwftvncz";
 const testCase2 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
 // first marker after character 10
 
+// Read in the input file
 const fs = require("fs");
-
 packet = fs.readFileSync("../2022/Day 6/input.txt", "utf8").split();
 
+// Function to check for unique characters
 const uniqueCharacterCheck = (sequence) => {
-  // We need to take the letter we're comparing out of the
-  // sequence and compare it with what's left in the sequence?
+  // Initialise a boolean variable to store the result of the check
+  // Assume the sequence is unique until proven otherwise
+  let isUnique = true;
 
-  // Let's just try taking the first letter and comparing it to the rest of the sequence
-  firstLetter = sequence[0];
+  // Loop through the sequence
+  for (
+    let comparisonIndex = 0;
+    comparisonIndex < sequence.length;
+    comparisonIndex++
+  ) {
+    // Store comparison letter in a variable
+    let comparisonLetter = sequence[comparisonIndex];
 
-  // use string.includes with the firstLetter as the search value and position [1] as the starting point.
-  if (sequence.includes(firstLetter, 1)) {
-    return false;
-  } else if ()
-  return true;
+    // Create the contents of the comparison string
+    let comparisonString = ``;
+    for (let stringIndex = 0; stringIndex < sequence.length; stringIndex++) {
+      if (comparisonIndex === stringIndex) {
+        continue;
+      } else comparisonString += sequence[stringIndex];
+    }
+
+    // Check variables are correct
+    // console.log(
+    //   `The comparison letter is ${comparisonLetter} and the corresponding sequence is ${comparisonString}`
+    // );
+
+    // Now we need to check each of the 4 letters against the comparison letter.
+    // If there is a match then this sequence cannot be the marker and therefor
+    // we must break out of this loop and move the sequence one letter along.
+    // If there is no match we need to keep going through each letter until we
+    // get to the end of the loop.
+
+    if (comparisonString.includes(comparisonLetter)) {
+      isUnique = false;
+      break;
+    }
+  }
+  return isUnique;
 };
 
 const packetStartMarker = (packet) => {
   // initialise required variables
   let sequence = "";
 
-  // First loop to act as the head of the sequence
+  // Generate the sequence that will be sent to the uniqueCharacterCheck function
+  // First loop to act as the head of the packet
   for (let head = 4; head < packet.length; head++) {
+
     // Second loop to act as the tail of the sequence
     for (let tail = head - 4; tail < head; tail++) {
       sequence += packet[tail];
     }
 
-    // Find the four different characters function should be in here.
-    // N.B. The marker that we're looking for as the final answer is the
-    // current position of the head when a unique sequence is found.
+    // Function to check for unique characters
+    // If it returns true, return the head position as the answer to Day 6 part 1
     if (uniqueCharacterCheck(sequence)) {
-      console.log(`Start-of-packet marker detected at position ${head}`)
-      break;
-    } else console.log(`No start-of-packet marker has been detected`);
+      console.log(`Start-of-packet marker detected at position ${head}`);
+      return head;
+    };
 
+    // Reset the sequence variable for the next loop
     sequence = "";
   }
 };
 
-packetStartMarker(testCase2);
+// Test cases
+// if (packetStartMarker(testCase1) === 5) {
+//   console.log(`Test case 1 passed`);
+// }
+
+// if (packetStartMarker(testCase2) === 10) {
+//   console.log(`Test case 2 passed`);
+// }
+
+console.log(packetStartMarker(packet.toString()));
+
 
 
 /* 
 So first off we have a sequence that only checks the first number against
 the rest of the sequence. This won't work if the sequence has the same letter 
 on the second and forth positions as is the case with testCase2 once the 
-loop has run once. So we now need to add an extra section of code to deal with 
-this 
+loop has run once. The trick was in where I had written the return statement  
+for the uniqueCharacterCheck function. It was inside the second loop and
+therefore returning a false positive as soon as it found a match. Moving it
+outside the loop was the fix.
 */
