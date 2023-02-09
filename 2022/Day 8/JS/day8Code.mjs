7731e3e092a74c2e6b3f094177559b651a67afe3
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-02-09 12:47:33 +0000

    Brainstorming

--- 2022/Day 8/JS/day8Code.mjs
+++ 2022/Day 8/JS/day8Code.mjs
@@ -2,12 +2,28 @@
 
 --- Day 8: Treetop Tree House ---
 
-- Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
-This points to another object  with key:value pairs
+- Each tree is represented as a single digit whose value is its height, where 
+  0 is the shortest and 9 is the tallest.
 
-- Tree is visible of all trees between it and edge of grid are SHORTER than it. 
+- Tree is visible if all trees between it and edge of grid are SHORTER than it. 
 
-- Only consider trees in in Cardinal direction
+- Only consider trees in in Cardinal direction for visibility
+
+  
+  OK so my thinking is to turn the input data into a series of rows and columns.
+
+  Remember that a tree is VISIBLE if all trees in cardinal directions are smaller
+  than it OR it is a tree on the outer edge.
+
+  Ignoring the first row (we know it's visible already), take the second row and
+  start at [1] then check if the numbers preceding and succeeding are smaller than
+  that number. If you get any hits then that tree is VISIBLE and you can stop 
+  checking. Add it to the total and go to the next tree on the line. If the number
+  in the row is NOT VISIBLE then we will need to perform a check of that number's 
+  column by using the row [index] as the number to find which column we need to check.
+  
+  At the end, add up all the visible trees from the above in addition to the ones
+  in the outer-most layer of the forest and that should give us the answer. 
 
 
 */
@@ -15,18 +31,17 @@ This points to another object  with key:value pairs
 
 import { readFileSync } from "node:fs";
 
-const testData = readFileSync("../2022/Day 7/testinput.txt", "utf8")
+const testDataPart1 = readFileSync("../2022/Day 8/testdata.txt", "utf8")
   .replace(/\r/g, "")
   .split("\n");
 // Part 1 result is 21 for this data
 
 const part1 = () => {
-  console.log(testData)
+  console.log(testDataPart1)
 }
 
 const part2 = () => {
-  const input = getInput();
-  //do something here
+
 }
 
 part1();
