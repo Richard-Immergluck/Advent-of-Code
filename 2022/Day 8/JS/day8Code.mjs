8c874752a7b7cacb1430bc14e8f27dfb59b3b074
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2023-02-11 23:56:23 +0000

    Day 8 part 1 completed

--- 2022/Day 8/JS/day8Code.mjs
+++ 2022/Day 8/JS/day8Code.mjs
@@ -9,7 +9,6 @@
 
 - Only consider trees in in Cardinal direction for visibility
 
-  
   OK so my thinking is to turn the input data into a series of rows and columns.
 
   Remember that a tree is VISIBLE if all trees in cardinal directions are smaller
@@ -25,125 +24,71 @@
   At the end, add up all the visible trees from the above in addition to the ones
   in the outer-most layer of the forest and that should give us the answer. 
 
+  I've spent a while on this and looked at some other solutions and what seems to 
+  be a common thread is to use a matrix/array of arrays. Then by basically turning
+  the forest into a grid with x and y axes you can then iterate over the whole area
+  checking each tree using it's coordinates and a preset maximum height. Basically,
+  if you can see a tree then the max hight is set to that tree and then you move on
+  to the next one in the line only if it's bigger than the previous max height then
+  it will be visible.  
 
 */
 
 import { readFileSync } from "node:fs";
 
-const testDataPart1 = readFileSync("../2022/Day 8/testdata.txt", "utf8")
+const lines = readFileSync("../2022/Day 8/testData.txt", "utf8")
   .replace(/\r/g, "")
+  .trim()
   .split("\n")
-  .toString()
-  .split(",");
-// Part 1 result is 21 for this data
+  .map((line) => [...line].map(Number)); // The map function returns an array of
+// arrays and then each entry in the
+// arrays is turned into a number
 
-const calcBorder = (forest) => {
-  const length = forest.length;
-  const width = forest[0].length;
-
-  const totalForestSize = length * width;
-  const innerForestSize = (length - 2) * (width - 2);
-  const borderTreeCount = totalForestSize - innerForestSize;
-
-  return borderTreeCount;
+// Simple function to add the visible tree to the set (visible)
+const setVisible = (y, x, visible) => {
+  visible.add(`${y}-${x}`);
 };
 
-const innerForestVisibilityCheck = (forest) => {
-  let visibleTrees = 0;
-
-  /*
-  Ignoring the first row (we know it's visible already), take the second row and
-  start at [1] then check if the numbers preceding and succeeding are smaller than
-  that number. If you get any hits then that tree is VISIBLE and you can stop 
-  checking. Add it to the total and go to the next tree on the line. If the number
-  in the row is NOT VISIBLE then we will need to perform a check of that number's 
-  column by using the row [index] as the number to find which column we need to check.
-*/
-
-  // iterate through each row of the forest and then each tree of the row
-  for (let row = 1; row < forest.length; row++) {
-    console.log(`The current row is ${forest[row]}`);
-    let precedingTrees = [];
-    let treeBuffer = [];
-
-    // populate the succeedingTrees variable
-    for (let tree = forest[row].length - 1; tree > 0; tree--) {
-      treeBuffer.push(forest[row][tree]);
+// Function to check rows and columns
+// We need the coordinates, the direction of movement, the dataset, and the visible SET
+const checkLine = (y, x, yDirection, xDirection, map, visible) => {
+  setVisible(y, x, visible); // We know that the first tree on all sides is visible
+  let maxHeight = map[y][x];
+  while (true) {
+    y += yDirection;
+    x += xDirection;
+    if (y < 0 || y >= map.length || x < 0 || x >= map[y].length) {
+      break;
     }
-
-    let succeedingTrees = treeBuffer.reverse();
-
-    for (let tree = 0; tree < forest[row].length - 1; tree++) {
-      let isVisible = false;
-      let currentTree = forest[row][tree];
-
-      if (tree !== 0) {
-        console.log(`The current tree being checked is ${forest[row][tree]}`);
-        console.log(`the preceding trees before check are ${precedingTrees}`);
-        console.log(`the succeeding trees before check are ${succeedingTrees}`);
-
-        for (let ptree = 0; ptree < precedingTrees.length; ptree++) {
-          if (forest[row][tree] > precedingTrees[ptree]) {
-            isVisible = true;
-            console.log(`This tree is visible from the WEST`);
-            break;
-          } else {
-            for (let stree = 0; stree < succeedingTrees.length; stree++) {
-              if (forest[row][tree] > succeedingTrees[stree]) {
-                isVisible = true;
-                console.log(`This tree is visible from the EAST`);
-                break;
-              }
-            }
-          }
-        }
-
-        // If the tree is not visible from either EAST or WEST then check North and South
-        if (!isVisible) {
-          console.log(`This tree is NOT VISIBLE from the EAST OR WEST`);
-
-          let column = [];
-
-          for (let row = 0; row < forest.length; row++) {
-            if (forest[row][tree] !== currentTree) {
-              column.push(forest[row][tree]);
-            }
-          }
-
-          console.log(
-            `the column for the tree that is not visible from EAST or WEST is ${column}`
-          );
-
-          for (const tree of column) {
-            if (tree < currentTree) {
-              isVisible = true;
-              break;
-            }
-          }
-        }
-      }
-
-      // Update the preceding and succeeding trees variables
-      precedingTrees.push(forest[row][tree]);
-      succeedingTrees.splice(0, 1);
+    if (map[y][x] > maxHeight) {
+      maxHeight = map[y][x];
+      setVisible(y, x, visible);
     }
-
-    console.log(`lets check if this is working?? ${isVisible}`)
-
-    break; // Working with one line only at the moment
   }
-
-  return visibleTrees;
 };
 
 const part1 = () => {
-  console.log(innerForestVisibilityCheck(testDataPart1));
-  // Answer to Part 1
-  // console.log(
-  //   `The number of trees visible from outside the grid are ${
-  //     calcBorder(testDataPart1) + innerForestVisibilityCheck(testDataPart1)
-  //   }`
-  // );
+  /* 
+    Use a set here to avoid duplication when using .add() 
+    From MDN:
+    The add() method inserts a new element with a specified value 
+    in to a Set object, if there isn't an element with the same 
+    value already in the Set.
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/add
+  */
+  const visible = new Set();
+  // all columns
+  for (let i = 0; i < lines[0].length; i++) {
+    checkLine(0, i, 1, 0, lines, visible);
+    checkLine(lines.length - 1, i, -1, 0, lines, visible);
+  }
+  // all rows
+  for (let i = 0; i < lines.length; i++) {
+    checkLine(i, 0, 0, 1, lines, visible);
+    checkLine(i, lines[0].length - 1, 0, -1, lines, visible);
+  }
+
+  console.log(visible.size);
 };
 
 const part2 = () => {};
